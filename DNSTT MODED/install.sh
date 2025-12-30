ddd637c2fb020dfdf864cdca9219b254bd20d21c
chiddy80 <rashidkamote95@gmail.com>
2025-12-30 15:19:03 +0000

    Update install.sh

--- DNSTT MODED/install.sh
+++ DNSTT MODED/install.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+# SLOWDNS INSTALLATION SCRIPT
+wget https://raw.githubusercontent.com/chiddy80/Halotel-Slow-DNS/main/DNSTT%20MODED/moded-slowdns.py
+wget https://raw.githubusercontent.com/chiddy80/Halotel-Slow-DNS/main/DNSTT%20MODED/config.ini
+python3 moded-slowdns.py --install
